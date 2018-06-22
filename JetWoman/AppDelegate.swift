5f3d720d7644a7a5227691921f7c3f27f7f6d3cc
Jason Wong <jawong71@gmail.com>
2018-06-22 11:47:51 -0700

    Initial Commit

---
+++ JetWoman/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  JetWoman
+//
+//  Created by Jason Wong on 2018-06-22.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
